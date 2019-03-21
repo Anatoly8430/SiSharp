0080add0052016a9143403260d1c89fd3df88124
Anat <anatt88@mail.ru>
2019-03-21 23:14:24 +0300

    Предварительный просмотр

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiHarp
+{/// <summary>
+/// Вспомогательная библиотека
+/// </summary>
+    class Class1
+    {
+        static public void SayHi()
+        {
+            Console.WriteLine(" Привет, я гик!");
+        }
+
+    }
+}
