70c296a06923dac6755f12902e90695612386fc9
Anat <anatt88@mail.ru>
2019-03-25 22:42:48 +0300

    Task 2.Написан метод подсчета количества цифр числа.

---
+++ Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Введите число a: ");
+            long a = long.Parse(Console.ReadLine());
+            int n = 0;
+            while (a > 0)
+            {
+                a = a / 10;
+                n++;
+            }
+            Console.Write(" Количество цифр  в числе составляет: " + n);
+            Console.ReadLine();
+        }
+    }
+}
